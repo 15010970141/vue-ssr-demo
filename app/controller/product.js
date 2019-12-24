22886476b81aee3a5b5b834a179622d57c91aa16
wangmm <wangmm2016@9186.com>
2019-12-24 18:07:03 +0800

    update .gitignore

--- app/controller/product.js
+++
@@ -1,9 +0,0 @@
-const Controller=require('egg').Controller;
-
-class ProductController extends Controller{
-    async index(){
-        const {ctx}=this;
-        ctx.body="product";//可以为str，也可以是object
-    }
-}
-module.exports=ProductController
